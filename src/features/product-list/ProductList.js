f4b1250e509c1e4d8d782aea6702edaf7cdf20d9
Pritam Magdum <pritammagdum24@gmail.com>
2024-02-17 23:46:49 +0530

    Added Navbar to Navigate The Web Pages

--- src/features/product-list/ProductList.js
+++ src/features/product-list/ProductList.js
@@ -17,13 +17,7 @@ const sortOptions = [
   { name: 'Price: Low to High', href: '#', current: false },
   { name: 'Price: High to Low', href: '#', current: false },
 ]
-const subCategories = [
-  { name: 'Totes', href: '#' },
-  { name: 'Backpacks', href: '#' },
-  { name: 'Travel Bags', href: '#' },
-  { name: 'Hip Bags', href: '#' },
-  { name: 'Laptop Sleeves', href: '#' },
-]
+
 const filters = [
   {
     id: 'color',
@@ -156,16 +150,6 @@ export default function ProductList() {
 
                       {/* Filters */}
                       <form className="mt-4 border-t border-gray-200">
-                        <h3 className="sr-only">Categories</h3>
-                        <ul role="list" className="px-2 py-3 font-medium text-gray-900">
-                          {subCategories.map((category) => (
-                            <li key={category.name}>
-                              <a href={category.href} className="block px-2 py-3">
-                                {category.name}
-                              </a>
-                            </li>
-                          ))}
-                        </ul>
 
                         {filters.map((section) => (
                           <Disclosure as="div" key={section.id} className="border-t border-gray-200 px-4 py-6">
@@ -287,14 +271,6 @@ export default function ProductList() {
                 <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
                   {/* Filters */}
                   <form className="hidden lg:block">
-                    <h3 className="sr-only">Categories</h3>
-                    <ul role="list" className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900">
-                      {subCategories.map((category) => (
-                        <li key={category.name}>
-                          <a href={category.href}>{category.name}</a>
-                        </li>
-                      ))}
-                    </ul>
 
                     {filters.map((section) => (
                       <Disclosure as="div" key={section.id} className="border-b border-gray-200 py-6">
