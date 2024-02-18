79976554dfd281fa04cd7e58e86bc0a099e7d441
Pritam Magdum <pritammagdum24@gmail.com>
2024-02-18 12:33:10 +0530

    Added ProductList and Category Section

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -67,13 +67,13 @@ function Navbar({ children }) {
                                         <div className="ml-4 flex items-center md:ml-6">
                                             <button
                                                 type="button"
-                                                className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                                                className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                                             >
                                                 <span className="absolute -inset-1.5" />
                                                 <span className="sr-only">View notifications</span>
                                                 <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
                                             </button>
-
+                                            <span className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium mb-7 -ml-3 text-red-700 ring-1 ring-inset ring-red-600/10">3</span>
                                             {/* Profile dropdown */}
                                             <Menu as="div" className="relative ml-3">
                                                 <div>
@@ -156,12 +156,12 @@ function Navbar({ children }) {
                                         </div>
                                         <button
                                             type="button"
-                                            className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                                            className="ml-auto flex-shrink-0 rounded-full  p-1 bg-gray-800 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                                         >
                                             <span className="absolute -inset-1.5" />
-                                            <span className="sr-only">View notifications</span>
                                             <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
                                         </button>
+                                        <span className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 mb-7 -ml-3 ring-1 ring-inset ring-red-600/10">3</span>
                                     </div>
                                     <div className="mt-3 space-y-1 px-2">
                                         {userNavigation.map((item) => (
@@ -183,7 +183,7 @@ function Navbar({ children }) {
 
                 <header className="bg-white shadow">
                     <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                        <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+                        <h1 className="text-3xl font-bold tracking-tight text-gray-900">E-Commerce</h1>
                     </div>
                 </header>
                 <main>
--- src/features/product-list/ProductList.js
+++ src/features/product-list/ProductList.js
@@ -1,11 +1,11 @@
 import React, { useState, Fragment } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import {
   increment,
   incrementAsync,
   selectCount,
 } from './productListSlice';
-import { useDispatch, useSelector } from 'react-redux';
-
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid'
 import { Dialog, Disclosure, Menu, Transition } from '@headlessui/react'
 import { XMarkIcon } from '@heroicons/react/24/outline'
 import { ChevronDownIcon, FunnelIcon, MinusIcon, PlusIcon, Squares2X2Icon } from '@heroicons/react/20/solid'
@@ -97,12 +97,17 @@ const products = [
     price: '$35',
     color: 'Black',
   },
+  // More products...
 ]
-export default function ProductList() {
+export default function Counter() {
+
+  const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false)
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
 
-  const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false)
+
+
+
 
   return (
     <div>
@@ -317,45 +322,105 @@ export default function ProductList() {
                   </form>
 
                   {/* Product grid */}
-                  <div className="lg:col-span-3">        {/* Product List Body */}
-                    <div className="bg-white">
-                      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-                        <h2 className="text-2xl font-bold tracking-tight text-gray-900">Products</h2>
+                  <div className="lg:col-span-3"><div className="bg-white">
+                    <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+                      <h2 className="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
 
-                        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                          {products.map((product) => (
-                            <div key={product.id} className="group relative">
-                              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                                <img
-                                  src={product.imageSrc}
-                                  alt={product.imageAlt}
-                                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                                />
-                              </div>
-                              <div className="mt-4 flex justify-between">
-                                <div>
-                                  <h3 className="text-sm text-gray-700">
-                                    <a href={product.href}>
-                                      <span aria-hidden="true" className="absolute inset-0" />
-                                      {product.name}
-                                    </a>
-                                  </h3>
-                                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
-                                </div>
-                                <p className="text-sm font-medium text-gray-900">{product.price}</p>
-                              </div>
+                      <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                        {products.map((product) => (
+                          <div key={product.id} className="group relative">
+                            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                              <img
+                                src={product.imageSrc}
+                                alt={product.imageAlt}
+                                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                              />
                             </div>
-                          ))}
-                        </div>
+                            <div className="mt-4 flex justify-between">
+                              <div>
+                                <h3 className="text-sm text-gray-700">
+                                  <a href={product.href}>
+                                    <span aria-hidden="true" className="absolute inset-0" />
+                                    {product.name}
+                                  </a>
+                                </h3>
+                                <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                              </div>
+                              <p className="text-sm font-medium text-gray-900">{product.price}</p>
+                            </div>
+                          </div>
+                        ))}
                       </div>
                     </div>
-                  </div>
+                  </div></div>
                 </div>
               </section>
+
+              <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+                <div className="flex flex-1 justify-between sm:hidden">
+                  <a
+                    href="#"
+                    className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+                  >
+                    Previous
+                  </a>
+                  <a
+                    href="#"
+                    className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+                  >
+                    Next
+                  </a>
+                </div>
+                <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+                  <div>
+                    <p className="text-sm text-gray-700">
+                      Showing <span className="font-medium">1</span> to <span className="font-medium">10</span> of{' '}
+                      <span className="font-medium">97</span> results
+                    </p>
+                  </div>
+                  <div>
+                    <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
+                      <a
+                        href="#"
+                        className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                      >
+                        <span className="sr-only">Previous</span>
+                        <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+                      </a>
+                      {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
+                      <a
+                        href="#"
+                        aria-current="page"
+                        className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      >
+                        1
+                      </a>
+                      <a
+                        href="#"
+                        className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                      >
+                        2
+                      </a>
+                      <a
+                        href="#"
+                        className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
+                      >
+                        3
+                      </a>
+                      <a
+                        href="#"
+                        className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                      >
+                        <span className="sr-only">Next</span>
+                        <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+                      </a>
+                    </nav>
+                  </div>
+                </div>
+              </div>
             </main>
           </div>
         </div>
-
 
 
       </div>
