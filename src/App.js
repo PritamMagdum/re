09fd7dfd9dddad981579ec381b5376bb4efb784c
Pritam Magdum <pritammagdum24@gmail.com>
2024-02-18 16:11:05 +0530

    Added Login and Signup Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 function App() {
   return (
     <div className="App">
       <Home></Home>
+      <LoginPage></LoginPage>
+      <SignupPage></SignupPage>
     </div>
   );
 }
