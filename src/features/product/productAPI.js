9db00850aa79da1a22496f33709383a5dcc14b03
Pritam Magdum <pritammagdum24@gmail.com>
2024-02-19 12:25:02 +0530

    Added Filter functionality on Products with the help of Redux

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -7,3 +7,19 @@ export function fetchAllProducts() {
     resolve({ data });
   });
 }
+
+export function fetchProductsByFilters(filter) {
+  // TODO : On server we will support multi values
+  // Suppose filter is comming like  {"category":"smartphone"}
+  let queryString = "";
+  for (let key in filter) {
+    queryString += `${key}=${filter[key]}&`
+  }
+
+  return new Promise(async (resolve) => {
+    // TODO : we will not hard-code server URL here
+    const responce = await fetch('http://localhost:8080/products?' + queryString);
+    const data = await responce.json();
+    resolve({ data });
+  });
+}
