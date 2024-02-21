54b56c5e2fbb0a988bd7023efe03a1d6e35953a7
Pritam Magdum <pritammagdum24@gmail.com>
2024-02-21 10:06:17 +0530

    Added Previous Page and Next Page Buttons Functionality

--- src/features/product/components/ProductList.js
+++ src/features/product/components/ProductList.js
@@ -3,12 +3,12 @@ import { useSelector, useDispatch } from 'react-redux';
 import {
   fetchBrandsAsync,
   fetchCategoriesAsync,
-  fetchFiltersAsync,
+  // fetchFiltersAsync,
   fetchProductsByFiltersAsync,
   selectAllProducts,
   selectBrands,
   selectCategories,
-  selectFilters,
+  // selectFilters,
   selectTotalItems,
 } from '../productSlice';
 
@@ -106,6 +106,7 @@ export default function ProductList() {
     dispatch(fetchBrandsAsync());
     dispatch(fetchCategoriesAsync());
     // dispatch(fetchFiltersAsync())
+    // eslint-disable-next-line
   }, []);
 
 
@@ -381,21 +382,21 @@ function DesktopFilter({ filters, handleFilter, categories, fetchBrandsData }) {
 
 
 function Pagination({ page, totalItems, handlePage }) {
+
+  const totalPages = Math.ceil(totalItems / ITEMS_PER_PAGE);
   return (
     <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
       <div className="flex flex-1 justify-between sm:hidden">
-        <a
-          href="#"
+        <div
           className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
         >
           Previous
-        </a>
-        <a
-          href="#"
+        </div>
+        <div
           className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
         >
           Next
-        </a>
+        </div>
       </div>
       <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
         <div>
@@ -406,15 +407,15 @@ function Pagination({ page, totalItems, handlePage }) {
         </div>
         <div>
           <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-            <a
-              href="#"
+            <div
+              onClick={e => handlePage(page > 1 ? page - 1 : page)}
               className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
               <span className="sr-only">Previous</span>
               <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-            </a>
+            </div>
             {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
-            {Array.from({ length: Math.ceil(totalItems / ITEMS_PER_PAGE) }).map((e, index) => (
+            {Array.from({ length: totalPages }).map((e, index) => (
               <div
                 onClick={e => handlePage(index + 1)}
                 aria-current="page"
@@ -424,15 +425,13 @@ function Pagination({ page, totalItems, handlePage }) {
               </div>
             ))}
 
-
-
-            <a
-              href="#"
+            <div
+              onClick={e => handlePage(page < totalPages ? page + 1 : page)}
               className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
               <span className="sr-only">Next</span>
               <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-            </a>
+            </div>
           </nav>
         </div>
       </div>
