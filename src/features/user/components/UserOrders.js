1cab84aab5c9d5dcac3ded89d812640465b566d5
Pritam <pritammagdum24@gmail.com>
2024-02-24 22:09:20 +0530

    Added Functionality to Fetch User Orders at My Orders Page

--- src/features/user/components/UserOrders.js
+++ src/features/user/components/UserOrders.js
@@ -13,8 +13,78 @@ export default function UserOrders() {
 
   return (
     <div>
-      {orders.map((order) => (
-        <div>{order.id}</div>
+      {orders.map((order, index) => (
+        <>
+          <div key={index}>
+            {/* {!items.length && <Navigate to="/" replace={true}></Navigate>} */}
+            <div className="mx-auto max-w-7xl px-4 bg-white sm:px-6 lg:px-8">
+              <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+                <h1 className="text-4xl font-bold tracking-tight my-3 text-gray-900">
+                  Order # {order.id}
+                </h1>
+                <h3 className="text-xl font-bold tracking-tight my-2 text-red-500">
+                  Order Status : {order.status}
+                </h3>
+                <hr className="my-5" />
+                <div className="flow-root">
+                  <ul role="list" className="-my-6 divide-y divide-gray-200">
+                    {order.items.map((item) => (
+                      <li key={item.id} className="flex py-6">
+                        <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                          <img
+                            src={item[0].thumbnail}
+                            alt={item[0].title}
+                            className="h-full w-full object-cover object-center"
+                          />
+                        </div>
+
+                        <div className="ml-4 flex flex-1 flex-col">
+                          <div>
+                            <div className="flex justify-between text-base font-medium text-gray-900">
+                              <h3>
+                                <a href={item.href}>{item.title}</a>
+                              </h3>
+                              <p className="ml-4">${item[0].price}</p>
+                            </div>
+                            <p className="mt-1 text-sm text-gray-500">
+                              {item.brand}
+                            </p>
+                          </div>
+                          <div className="flex flex-1 items-end justify-between text-sm">
+                            <div className="text-gray-500">
+                              <label
+                                htmlFor="quantity"
+                                className="inline text-sm font-medium leading-6 text-gray-900"
+                              >
+                                Qty {order.quantity}
+                              </label>
+                            </div>
+
+                            <div className="flex"></div>
+                          </div>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+
+              <div className="border-t border-gray-200 px-4 py-6 sm:px-6 my-3">
+                <div className="flex justify-between text-base font-medium text-gray-900">
+                  <p>Subtotal</p>
+                  <p>${order.totalAmount}</p>
+                </div>
+                <div className="flex justify-between text-base font-medium text-gray-900 my-3">
+                  <p>Total Items in Cart</p>
+                  <p>{order.totalItems}</p>
+                </div>
+                <p className="mt-0.5 text-sm text-gray-500">
+                  Shipping and taxes calculated at checkout.
+                </p>
+              </div>
+            </div>
+          </div>
+        </>
       ))}
     </div>
   );
