6225dc993f9f80547a85e1ccb848447db920ba5e
Pritam <pritammagdum24@gmail.com>
2024-02-27 11:27:07 +0530

    Added Table for Showing Fetched Orders

--- src/features/admin/components/AdminOrders.js
+++ src/features/admin/components/AdminOrders.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { ITEMS_PER_PAGE } from "../../../app/constants";
+import { PencilIcon, EyeIcon } from "@heroicons/react/24/outline";
 import {
   fetchAllOrdersAsync,
   selectOrders,
@@ -15,8 +16,8 @@ function AdminOrders() {
 
   useEffect(() => {
     const pagination = { _page: page, _per_page: ITEMS_PER_PAGE };
+    console.log("This is pagination -->", pagination);
     dispatch(fetchAllOrdersAsync(pagination));
-    // dispatch(fetchAllProductsAsync())
   }, [dispatch, page]);
 
   return (
@@ -28,13 +29,13 @@ function AdminOrders() {
               <table className="min-w-max w-full table-auto">
                 <thead>
                   <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-                    <th className="py-3 px-6 text-left">Order Number</th>
+                    <th className="py-3 px-6 text-left">Order</th>
                     <th className="py-3 px-6 text-left">Items</th>
                     <th className="py-3 px-6 text-center">Total Amount</th>
+                    <th className="py-3 px-6 text-center">Shipping Address</th>
                     <th className="py-3 px-6 text-center">Status</th>
-                    {/* <th className="py-3 px-6 text-center">Actions</th> */}
                     <th className="py-3 px-6 text-center">
-                      {console.log(orders)}
+                      {console.log("this is orders -->", orders)}
                     </th>
                   </tr>
                 </thead>
@@ -45,7 +46,7 @@ function AdminOrders() {
                         <td className="py-3 px-6 text-left whitespace-nowrap">
                           <div className="flex items-center">
                             <div className="mr-2"></div>
-                            <span className="font-medium">React Project</span>
+                            <span className="font-medium">{order.id}</span>
                           </div>
                         </td>
                         <td className="py-3 px-6 text-left">
@@ -57,7 +58,10 @@ function AdminOrders() {
                                   src={item[0].thumbnail}
                                 />
                               </div>
-                              <span>{item[0].title}</span>
+                              <span>
+                                {item[0].title} - #{item.quantity} - $
+                                {item[0].price}
+                              </span>
                             </div>
                           ))}
                         </td>
@@ -67,64 +71,24 @@ function AdminOrders() {
                           </div>
                         </td>
                         <td className="py-3 px-6 text-center">
+                          <div className="">
+                            <div>
+                              <strong>{order.selectedAddress.name}</strong>,
+                            </div>
+                            <div>{order.selectedAddress.street},</div>
+                            <div>{order.selectedAddress.city},</div>
+                            <div>{order.selectedAddress.state},</div>
+                            <div>{order.selectedAddress.pinCode},</div>
+                            <div>{order.selectedAddress.phone}</div>
+                          </div>
+                        </td>
+                        <td className="py-3 px-6 text-center">
                           <span className="bg-purple-200 text-purple-600 py-1 px-3 rounded-full text-xs">
                             {order.status}
                           </span>
                         </td>
                         <td className="py-3 px-6 text-center">
-                          <div className="flex item-center justify-center">
-                            <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                              >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  strokeWidth={2}
-                                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                                />
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  strokeWidth={2}
-                                  d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                                />
-                              </svg>
-                            </div>
-                            <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                              >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  strokeWidth={2}
-                                  d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-                                />
-                              </svg>
-                            </div>
-                            <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                              >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  strokeWidth={2}
-                                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                                />
-                              </svg>
-                            </div>
-                          </div>
+                          <div className="flex item-center justify-center"></div>
                         </td>
                       </tr>
                     ))}
