fb3b3148118ac1da50b70e3defacb6f79589eeb6
Pritam <pritammagdum24@gmail.com>
2024-03-10 23:44:07 +0530

    Fixed Order Number Size, margin and padding according to the Mobile Devices

--- src/pages/OrderSuccessPage.js
+++ src/pages/OrderSuccessPage.js
@@ -29,7 +29,7 @@ function OrderSuccessPage() {
           <p className="text-base font-semibold text-indigo-600">
             Order Successfully Placed
           </p>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+          <h1 className="mt-4 text-lg md:text-2xl flex font-bold tracking-tight text-gray-900 lg:text-4xl">
             Order Number : #{params?.id}
           </h1>
           <p className="mt-6 text-base leading-7 text-gray-600">
