47856bd274638c11fc79ab3ad94847cf4fca239e
Pritam <pritammagdum24@gmail.com>
2024-03-10 10:40:55 +0530

    Added Navbar in Order Success Page and Added Link to the Contact Support Button at Order Success Page

--- src/pages/OrderSuccessPage.js
+++ src/pages/OrderSuccessPage.js
@@ -4,6 +4,7 @@ import { Link, Navigate, useParams } from "react-router-dom";
 import { resetCartAsync } from "../features/cart/cartSlice";
 import { selectLoggedInUser } from "../features/auth/authSlice";
 import { resetOrder } from "../features/order/orderSlice";
+import Navbar from "../features/Navbar/Navbar";
 
 function OrderSuccessPage() {
   const params = useParams();
@@ -20,9 +21,10 @@ function OrderSuccessPage() {
   }, [dispatch]);
 
   return (
-    <>
+    <div>
       {!params.id && <Navigate to="/" replace={true}></Navigate>}
-      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <Navbar></Navbar>
+      <main className="grid min-h-full place-items-center bg-white px-6 -mt-10 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
           <p className="text-base font-semibold text-indigo-600">
             Order Successfully Placed
@@ -40,13 +42,16 @@ function OrderSuccessPage() {
             >
               Go back home
             </Link>
-            <a href="#" className="text-sm font-semibold text-gray-900">
-              Contact support <span aria-hidden="true">&rarr;</span>
-            </a>
+            <Link
+              to="/contact"
+              className="rounded-md bg-gray-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
+            >
+              Contact support
+            </Link>
           </div>
         </div>
       </main>
-    </>
+    </div>
   );
 }
 
